1bf1dfaedc230e2ca880271e7d80847b296aead5
Tom Buckley <thomasbuckley@gmail.com>
2020-05-16 22:16:06 +0100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "eu-west-2",
+        BUCKET: "app-notes-bucket-tom"
+    },
+    apiGateway: {
+        REGION: "eu-west-2",
+        URL: "https://9l5pzf69pg.execute-api.eu-west-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "eu-west-2",
+        USER_POOL_ID: "eu-west-2_op8MaklUO",
+        APP_CLIENT_ID: "1mng5na2sehf4crdgcg7p9l1h8",
+        IDENTITY_POOL_ID: "eu-west-2:fd32bd43-fb63-473b-b254-ed71aabb089e"
+    }
+};
